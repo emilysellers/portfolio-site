63700b241f81f40b250d631e2dff9abd01725717
Emily Sellers <emilymelindasellers@gmail.com>
2023-08-02 16:16:49 -0700

    Add project title, link to site and basic styling

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -3,8 +3,28 @@ import { getProject } from "@/sanity/sanity-utils";
 type Props = {
   params: { project: string };
 };
+
 export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
-  return <div>{project.name}</div>;
+
+  return (
+    <div className="max-w-3xl mc-auto py-20">
+      <header className="flex items-center justify-between">
+        <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl font-extrabold">
+          {project.name}
+        </h1>
+        <a
+          href={project.url}
+          title="View Site"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          View Site
+        </a>
+      </header>
+      {/* <div>{project.content}</div> */}
+    </div>
+  );
 }
