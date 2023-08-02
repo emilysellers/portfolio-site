1456f73e76b302b81786ec6ae91345b1aaea0c6d
Emily Sellers <emilymelindasellers@gmail.com>
2023-08-02 16:29:23 -0700

    Display project content and image on project details page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string };
@@ -24,7 +26,16 @@ export default async function Project({ params }: Props) {
           View Site
         </a>
       </header>
-      {/* <div>{project.content}</div> */}
+      <div className="text-lg text-gray-700 mt-5">
+        <PortableText value={project.content} />
+        <Image
+          src={project.image}
+          alt={project.image}
+          width={1920}
+          height={1080}
+          className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
+        ></Image>
+      </div>
     </div>
   );
 }
