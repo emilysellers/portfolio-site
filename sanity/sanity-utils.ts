9c92e5769176ef6a8ee3631403a6082d59fe6ec5
Emily Sellers <emilymelindasellers@gmail.com>
2023-08-16 14:57:58 -0700

    Declare `getPages` and `getPage` functions

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
   // use next-sanity package to create client that can read from our content link
@@ -28,6 +29,30 @@ export async function getProject(slug: string): Promise<Project> {
         url,
         content
     }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type ==  "page"]{
+      _id,
+      _createdAt,
+      title, 
+      "slug": slug.current
+    }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+    _id,
+      _createdAt,
+      title, 
+      "slug": slug.current,
+    content
+  }`,
     { slug }
   );
 }
